c456d6e74112a230829ef24fe8ed17b2b79931b6
Sebil Satici <hallo@sebil.dev>
2025-12-23 16:13:03 +0100

    add basename to browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/sam-rash.com">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
